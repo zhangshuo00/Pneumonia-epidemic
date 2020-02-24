077896f468b217c7a66feda06c598703626e98aa
zhangshuo <443494891@qq.com>
2020-02-24 17:13:35 +0800

    rn

--- ProjectName/App.js
+++ ProjectName/App.js
@@ -6,109 +6,108 @@
  * @flow
  */
 
-import React from 'react';
+import React,{useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
+  Image,
   View,
   Text,
   StatusBar,
+  ImageBackground,
+  TextInput,
+  Button,
+  TouchableOpacity
 } from 'react-native';
+import ShowMyName from './Components/ShowMyName'
+import Counter from './Components/Counter'
+import ImageBack from './Components/ImageBack'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [num, setNum] = useState('121');
 
-const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+        <ScrollView>
+
+          {/* flex布局 */}
+          {/* 在rn中，view默认设置了flex属性， */}
+          <View style={{flexDirection:'row',flexWrap:'wrap'}}>
+            <View style={styles.search}>
+              <Image style={{marginLeft:5,marginTop:5}} source={require('./assets/客户公海.png')}/>
+              <TextInput placeholder="搜索"/>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+            <TouchableOpacity style={{
+                width:40,
+                height:40,
+                backgroundColor:'#ADD8E6',
+                alignItems:'center',
+                justifyContent:'center',
+                borderRadius:20,
+                marginLeft:10,
+                marginTop:20
+              }}>
+              <Text>+</Text>  
+            </TouchableOpacity>
           </View>
+          
         </ScrollView>
+        {/* 受控组件 */}
+        {/* <Text>{num}</Text>
+        <TextInput
+          onChangeText={(val)=>{setNum(val)}}
+          style={{
+            borderColor:'black',
+            borderWidth:1,
+            borderRadius:10,
+          }}
+        /> */}
+        {/* <ImageBackground style={{width:300,height:300}} source={require('./cake.jpg')}>
+          <View style={styles.box}></View>
+          <Text style={[styles.txt]}>hello,world</Text>
+        </ImageBackground> */}
+        {/* 自定义背景图组件 */}
+        {/* <ImageBack style={{width:300,height:300}} source={require('./cake.jpg')}>
+          <View style={styles.box}></View>
+          <Text style={[styles.txt]}>hello,world</Text>
+        </ImageBack> */}
+        {/* <View style={styles.box}>
+          <Text style={styles.txt}>
+            <Text>inner1</Text>
+            <Text>inner2</Text>
+          </Text>
+        </View>
+        <View>
+          <Image resizeMode="cover" source={require('./assets/客户公海.png')}/>
+        </View> */}
       </SafeAreaView>
     </>
   );
 };
-
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  box:{
+    width:100,
+    height:100,
+    marginBottom:10,
+    backgroundColor:'yellow'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  txt:{
+    fontSize:20,
+    color:'white'
   },
-  body: {
-    backgroundColor: Colors.white,
+  input:{
+    width:200,height:40,borderColor:'black',borderWidth:0.5,borderRadius:10
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  search:{
+    flexDirection:'row',borderColor:'black',borderWidth:1,borderRadius:20,
+    width:250,
+    height:50,
+    marginLeft:10,
+    marginTop:20
+  }
+})
 
 export default App;
