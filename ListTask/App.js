ee99b2da3e223fab33c824f119cdeeee1cbc2e35
zhangshuo <443494891@qq.com>
2020-03-12 12:06:43 +0800

    雪梨任务

--- ListTask/App.js
+++ ListTask/App.js
@@ -6,37 +6,13 @@ import {
   Text,
   TextInput,
 } from 'react-native';
-import { Icon } from '@ant-design/react-native';
-import Prodescrip from './Components/Prodescrip'
 
 const App = () => {
   return (
-    <ScrollView style={{backgroundColor:'#f4f4f4'}}>
-      <View style={{marginTop:10,justifyContent:'center',flexDirection:'row',flexWrap:'wrap'}}>
-        <TextInput placeholder="请输入商品名称" style={styles.search}/>
-        <Icon style={{position:'absolute',right:50,top:8}} name="search"/>
-      </View>
-      <View style={{marginTop:15,marginBottom:10,justifyContent:'space-around',flexDirection:'row',flexWrap:'wrap'}}>
-        <Text style={{color:'red'}}>综合</Text>
-        <Text>销量</Text>
-        <Text>新品</Text>
-        <Text>价格</Text>
-        <Text>信用</Text>
-      </View>
-      <Prodescrip/>
-      <Prodescrip/>
-      <Prodescrip/>
-    </ScrollView>
+      <Router>
+        
+      </Router>
   );
 };
-
-const styles = StyleSheet.create({
-  search:{
-    width:'85%',
-    height:40,
-    backgroundColor:'#eeeeee',
-    borderRadius:10,
-  },
-});
 
 export default App;
---
+++ ProjectName/App4.js
@@ -0,0 +1,106 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  Image,
+  View,
+  Text,
+} from 'react-native'
+import {Scene, Router, Tabs, Drawer, Lightbox, Modal, Overlay} from 'react-native-router-flux'
+import Msg from './Components/Msg'
+import Doc from './Components/Doc'
+import Mybox from './Components/Mybox'
+import MsgDetails from './Components/MsgDetails'
+import ShowMyName from './Components/ShowMyName'
+import Home from './Components/Home'
+import Login from './Components/Login'
+import Message from './Components/Message'
+import { Grid, Icon } from '@ant-design/react-native';
+import { outlineGlyphMap } from '@ant-design/icons-react-native/lib/outline';
+import MyList from './Components/MyList';
+import LocalStorage from './Components/LocalStorage';
+import MyTs from './Components/MyTs';
+
+console.disableYellowBox = true;
+
+
+const App = () => {
+  return (
+      <Router>
+        <Overlay>
+        <Modal key="modal" hideNavBar>
+              <Lightbox key="lightbox">
+                <Drawer
+                  key="drawer"
+                  contentComponent={()=><Text>drawer</Text>}
+                  drawerIcon={()=><Icon name="menu"/>}
+                  drawerWidth={400}
+                >
+                  <Scene key="root">
+                    <Tabs key="tabbar" hideNavBar
+                      activeTintColor='red'
+                      inactiveTintColor='blue'
+                    >
+                      {/* 首页 */}
+                      <Scene 
+                        title="首页"
+                        icon={
+                          ({focused})=><Icon color={focused?'red':'green'} name="home"/>
+                        }
+                      >
+                        <Scene key='home' component={MyTs}/>
+                        <Scene key="mylist" component={MyList}/>
+                      </Scene>
+                      {/* 消息栏 */}
+                      <Scene 
+                        key='msg'
+                        title="消息"
+                        icon={
+                          ({focused})=><Icon color={focused?'red':'green'} name="home"/>
+                        }
+                      >
+                        <Scene key='ms' component={Msg}/>
+                        <Scene key='msgdetails' hideTabBar component={MsgDetails}/>
+                      </Scene>
+                      {/* 文档栏 */}
+                      <Scene 
+                        key='doc'
+                        title="文档"
+                        icon={
+                          ({focused})=><Icon color={focused?'red':'green'} name="file"/>
+                        }
+                      >
+                        <Scene key='docs' component={Doc}/>
+                      </Scene>
+                      <Scene key='storage'
+                        title="获取数据"
+                      >
+                        <Scene key='fetchdata' component={LocalStorage}/>
+                      </Scene>
+                    </Tabs>
+                  </Scene>
+                </Drawer>
+                <Scene key="light" component={Mybox}/>
+              </Lightbox>
+              <Scene key="login" component={Login}/>
+              <Scene key="login1" component={ShowMyName}/>
+        </Modal>
+        <Scene component={Message}/>      
+        </Overlay>
+      </Router>
+  );
+};
+const styles = StyleSheet.create({
+
+})
+
+export default App;
