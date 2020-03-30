bd8485a3a20345cda971ab5bae534a7e0410f994
zhangshuo <443494891@qq.com>
2020-03-30 22:15:51 +0800

    引导页&登录

--- ProjectName/App.js
+++ ProjectName/App.js
@@ -6,58 +6,124 @@
  * @flow
  */
 
-import React,{useEffect} from 'react';
+import React,{useState,useEffect} from 'react';
 import {
   StyleSheet,
-  ScrollView,
-  Image,
   View,
-  Text,
+  AsyncStorage,
+  BackHandler,
+  ToastAndroid,
 } from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
-import {Scene, Router, Tabs } from 'react-native-router-flux'
+import {Router, Overlay, Scene, Tabs, Drawer, Lightbox, Modal, Actions} from 'react-native-router-flux';
 import { Icon } from '@ant-design/react-native';
 import ListSort from './Components/ListSort'
 import Service from './Components/Service';
 import Center from './Components/Center';
+import Login from './Components/Login'
+import SwiperPage from './Components/SwiperPage'
 
 console.disableYellowBox = true;
 
 
 const App = () => {
+	let [isLogin, setIsLogin] = useState(false);
+	let [isInstall, setIsInstall] = useState(true);
+	let now = 0;
+	let init = ()=>{
+		AsyncStorage.getItem('isInstall')
+		.then(res=>{
+			console.log('isInstall',res)
+			if(res){
+				setIsInstall(false)
+			}
+		})
+		AsyncStorage.getItem('user')
+		.then(res=>{
+			let user = JSON.parse(res)
+			console.log(user)
+			if(!user){
+				SplashScreen.hide()
+			}
+			if(user&&user.token){
+				setIsLogin(true)
+				SplashScreen.hide()
+			}
+		})
+	}
 	useEffect(() => {
-		SplashScreen.hide()
+		init()
 	}, [])
+	let afterInstall = ()=>{
+		console.log('after install')
+		setIsInstall(false)
+	}
+	if(isInstall){
+		return (
+			<View style={{flex:1}}>
+				<SwiperPage afterInstall={afterInstall}/>
+			</View>
+		)
+	}
 	return (
-		<Router>
-			<Scene hideNavBar headerMode="none" key="root">
-				<Tabs>
-					<Scene
-						key="home"
-						component={Service}
-						title="首页"
-						icon={
-							({focused})=><Icon color={focused?'red':''} name="home"/>
-						}
-					/>
-					<Scene
-						key="sort"
-						component={ListSort}
-						title="分类"
-						icon={
-							({focused})=><Icon color={focused?'red':''} name="file"/>
-						}
-					/>
-					<Scene
-						key="personal"
-						component={Center}
-						title="个人中心"
-						icon={
-							({focused})=><Icon color={focused?'red':''} name="user"/>
-						}
-					/>
-				</Tabs>
-			</Scene>
+		<Router
+			backAndroidHandler={()=>{
+				if(Actions.currentScene != 'home'){
+					Actions.pop()
+					return true
+				}else{
+					if(new Date().getTime()-now<2000){
+						BackHandler.exitApp()
+					}else{
+						ToastAndroid.show('确定要退出吗',100)
+						now = new Date().getTime()
+						return true
+					}
+				}
+			}}
+		>
+			<Overlay>
+				<Modal key="modal" hideNavBar>
+					<Lightbox key="lightbox">
+						<Drawer 
+							key="drawer"
+							contentComponent={()=><Text>drawer</Text>}
+							drawerIcon={()=><Icon name="menu"/>}
+							drawerWidth={400}
+						>
+							<Scene hideNavBar headerMode="none" key="root">
+								<Tabs>
+									<Scene
+										key="home"
+										component={Service}
+										title="首页"
+										icon={
+											({focused})=><Icon color={focused?'red':''} name="home"/>
+										}
+									/>
+									<Scene
+										key="sort"
+										component={ListSort}
+										title="分类"
+										icon={
+											({focused})=><Icon color={focused?'red':''} name="file"/>
+										}
+									/>
+									<Scene
+										key="personal"
+										component={Center}
+										title="个人中心"
+										icon={
+											({focused})=><Icon color={focused?'red':''} name="user"/>
+										}
+									/>
+								</Tabs>
+							</Scene>
+						</Drawer>
+					</Lightbox>
+					<Scene initial={!isLogin} key="login" component={Login} />
+				</Modal>
+			</Overlay>
 		</Router>
 	);
 };
--- ProjectName/Components/Login.js
+++ ProjectName/Components/Login.js
@@ -1,40 +1,78 @@
-import React, { Component } from 'react'
-import { Text, View, Image, TextInput, TouchableOpacity } from 'react-native'
-import { Actions } from 'react-native-router-flux'
-import { Icon } from '@ant-design/react-native'
+import React, {Component} from 'react';
+import {View, Text, Image, TextInput, AsyncStorage, TouchableOpacity} from 'react-native';
+import { Icon } from '@ant-design/react-native';
+import { Actions } from 'react-native-router-flux';
+import {myFetch} from '../utils'
 
 export default class Login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            username:'',
+            pwd:'',
+            isLoading:false
+        }
+    }
+    userhandle = (text)=>{
+        this.setState({username:text})
+    }
+    pwdhandle = (text)=>{
+        this.setState({pwd:text})
+    }
+    login = ()=>{
+        this.setState({isLoading:true})
+        myFetch.post('/login',{
+            username:this.state.username,
+            pwd:this.state.pwd
+        }).then(res=>{
+            // 根据返回状态进行判断，正确时跳转首页
+            // if(res){
+
+            // }
+            AsyncStorage.setItem('user',JSON.stringify(res.data))
+            .then(()=>{
+                this.setState({isloading:false})
+                Actions.homePage();
+            })
+        })
+    }
     render() {
         return (
             <View style={{flex: 1,justifyContent: 'center'}}>
-                <View
-                style={{ alignItems: 'center'}}>
-                <View
-                    style={{
-                    width: '80%',
-                    marginRight: 10,
-                    borderBottomColor: '#ccc',
-                    borderBottomWidth: 1,
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    paddingLeft: 20,
-                    }}>
-                    <Icon name="user" color="red"/>
-                    <TextInput placeholder="搜索" />
-                </View>
-                <View
-                    style={{
-                    width: '80%',
-                    marginRight: 10,
-                    borderBottomColor: '#ccc',
-                    borderBottomWidth: 1,
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    paddingLeft: 20,
-                    }}>
-                    <Icon name="user" color="red"/>
-                    <TextInput placeholder="搜索" />
-                </View>
+                <View style={{ alignItems: 'center'}}>
+                    <View
+                        style={{
+                        width: '80%',
+                        marginRight: 10,
+                        borderBottomColor: '#ccc',
+                        borderBottomWidth: 1,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingLeft: 20,
+                        }}>
+                        <Icon name="user" color="red"/>
+                        <TextInput placeholder="用户名" 
+                            onChangeText={this.userhandle}
+                        />
+                    </View>
+                    <View
+                        style={{
+                        width: '80%',
+                        marginRight: 10,
+                        borderBottomColor: '#ccc',
+                        borderBottomWidth: 1,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingLeft: 20,
+                        }}
+                    >
+                        <Icon name="user" color="red"/>
+                        <TextInput 
+                            onChangeText={this.pwdhandle}
+                            placeholder="密码" 
+                            secureTextEntry={true}
+                        />
+                    </View>
                     <TouchableOpacity 
                         style={{
                             width: '80%',
@@ -44,10 +82,13 @@ export default class Login extends Component {
                             alignItems: 'center',
                             justifyContent: 'center'
                         }}
-                        onPress={Actions.pop}>
+                        onPress={this.login}>
                         <Text>登录</Text>
                     </TouchableOpacity>
                 </View>
+                {
+                    this.state.isloading ? <View><Text>正在登录。。。</Text></View> : null
+                }
             </View>
         )
     }
